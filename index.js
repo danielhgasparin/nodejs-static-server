339aa2b746138c508ce6202297533e2b6b069ee9
Daniel Henrique Gasparin <danielhgasparin@outlook.com>
2018-06-02 18:00:57 -0300

    Clean up code

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-var static = require('node-static');
+const http = require('http');
+const static = require('node-static');
 
-var fileServer = new static.Server('./public'); // Configure folder with the static content to serve
+const fileServer = new static.Server('./public'); // Configure folder with the static content to serve.
 
-require('http').createServer(function (request, response) {
+const server = http.createServer(function (request, response) {
     request.addListener('end', function () {
         fileServer.serve(request, response);
     }).resume();
-}).listen(8080); // Configure port to listen
+});
+
+server.listen(8080); // Configure port to listen.
